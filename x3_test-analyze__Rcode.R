1709bf5cd94419b2be6df0520216fbf79413e2ae
pavelVPo <97514993+pavelVPo@users.noreply.github.com>
2026-06-04 17:09:16 +0300

    Update x3_test-analyze__Rcode.R

--- x3_test-analyze__Rcode.R
+++ x3_test-analyze__Rcode.R
@@ -6,16 +6,153 @@ pairs <- read_tsv("C:/.../pairs_processed.tsv") |>
 symbols <- read_tsv("C:/.../symbols_&_info.tsv")
 
 ## Apply rules
-# atom_oar
-symb__atom_oar <- symbols |> filter(class == "atom_oar") |>
+# atom_oar / atom_bar
+symb__atom_obar <- symbols |> filter(class == "atom_oar" | class == "atom_bar") |>
 					select(symbols) |>
 					separate_longer_delim(symbols, delim = ", ") |>
 					distinct()
-data__atom_oar <- pairs |> inner_join(symb__atom_oar, by = c("target" = "symbols")) |>
-					bind_rows(pairs |> inner_join(symb__atom_oar, by = c("found_target" = "symbols"))) |>
+data__atom_obar <- pairs |> inner_join(symb__atom_obar, by = c("target" = "symbols")) |>
+					bind_rows(pairs |> inner_join(symb__atom_obar, by = c("found_target" = "symbols"))) |>
+					distinct()
+# atom_oal / atom_bal
+symb__atom_obal <- symbols |> filter(class == "atom_oal" | class == "atom_bal") |>
+					select(symbols) |>
+					separate_longer_delim(symbols, delim = ", ") |>
+					distinct()
+data__atom_obal <- pairs |> inner_join(symb__atom_obal, by = c("target" = "symbols")) |>
+					bind_rows(pairs |> inner_join(symb__atom_obal, by = c("found_target" = "symbols"))) |>
+					distinct()
+# atom_oal_2 / atom_bal_2
+symb__atom_obal_2 <- symbols |> filter(class == "atom_oal_2" | class == "atom_bal_2") |>
+					select(symbols) |>
+					separate_longer_delim(symbols, delim = ", ") |>
+					distinct()
+data__atom_obal_2 <- pairs |> inner_join(symb__atom_obal_2, by = c("target" = "symbols")) |>
+					bind_rows(pairs |> inner_join(symb__atom_obal_2, by = c("found_target" = "symbols"))) |>
+					distinct()
+# anything
+symb__any <- symbols |> filter(class == "anything" | class == "anything") |>
+					select(symbols) |>
+					separate_longer_delim(symbols, delim = ", ") |>
+					distinct()
+data__any <- pairs |> inner_join(symb__any, by = c("target" = "symbols")) |>
+					bind_rows(pairs |> inner_join(symb__any, by = c("found_target" = "symbols"))) |>
+					distinct()
+# bracket
+symb__br <- symbols |> filter(class == "s_bracket" | class == "l_bracket") |>
+					select(symbols) |>
+					separate_longer_delim(symbols, delim = ", ") |>
+					distinct()
+data__br <- pairs |> inner_join(symb__br, by = c("target" = "symbols")) |>
+					bind_rows(pairs |> inner_join(symb__br, by = c("found_target" = "symbols"))) |>
+					distinct()
+# single bond
+symb__sb <- symbols |> filter(class == "single_bond") |>
+					select(symbols) |>
+					separate_longer_delim(symbols, delim = ", ") |>
+					distinct()
+data__sb <- pairs |> inner_join(symb__sb, by = c("target" = "symbols")) |>
+					bind_rows(pairs |> inner_join(symb__sb, by = c("found_target" = "symbols"))) |>
+					distinct()
+# double bond
+symb__db <- symbols |> filter(class == "double_bond") |>
+					select(symbols) |>
+					separate_longer_delim(symbols, delim = ", ") |>
+					distinct()
+data__db <- pairs |> inner_join(symb__db, by = c("target" = "symbols")) |>
+					bind_rows(pairs |> inner_join(symb__db, by = c("found_target" = "symbols"))) |>
+					distinct()
+# triple bond
+symb__tb <- symbols |> filter(class == "triple_bond") |>
+					select(symbols) |>
+					separate_longer_delim(symbols, delim = ", ") |>
+					distinct()
+data__tb <- pairs |> inner_join(symb__tb, by = c("target" = "symbols")) |>
+					bind_rows(pairs |> inner_join(symb__tb, by = c("found_target" = "symbols"))) |>
+					distinct()
+# quadruple bond
+symb__qb <- symbols |> filter(class == "quadruple_bond") |>
+					select(symbols) |>
+					separate_longer_delim(symbols, delim = ", ") |>
+					distinct()
+data__qb <- pairs |> inner_join(symb__qb, by = c("target" = "symbols")) |>
+					bind_rows(pairs |> inner_join(symb__qb, by = c("found_target" = "symbols"))) |>
+					distinct()
+# aromatic bond
+symb__ab <- symbols |> filter(class == "aromatic_bond") |>
+					select(symbols) |>
+					separate_longer_delim(symbols, delim = ", ") |>
+					distinct()
+data__ab <- pairs |> inner_join(symb__ab, by = c("target" = "symbols")) |>
+					bind_rows(pairs |> inner_join(symb__ab, by = c("found_target" = "symbols"))) |>
+					distinct()
+# aromatic bond
+symb__ab <- symbols |> filter(class == "aromatic_bond") |>
+					select(symbols) |>
+					separate_longer_delim(symbols, delim = ", ") |>
+					distinct()
+data__ab <- pairs |> inner_join(symb__ab, by = c("target" = "symbols")) |>
+					bind_rows(pairs |> inner_join(symb__ab, by = c("found_target" = "symbols"))) |>
+					distinct()
+# no bond
+symb__nb <- symbols |> filter(class == "no_bond") |>
+					select(symbols) |>
+					separate_longer_delim(symbols, delim = ", ") |>
+					distinct()
+data__nb <- pairs |> inner_join(symb__nb, by = c("target" = "symbols")) |>
+					bind_rows(pairs |> inner_join(symb__nb, by = c("found_target" = "symbols"))) |>
+					distinct()
+# branch
+symb__branch <- symbols |> filter(class == "bm_ibi" | class == "bm_tbi") |>
+					select(symbols) |>
+					separate_longer_delim(symbols, delim = ", ") |>
+					distinct()
+data__branch <- pairs |> inner_join(symb__branch, by = c("target" = "symbols")) |>
+					bind_rows(pairs |> inner_join(symb__branch, by = c("found_target" = "symbols"))) |>
+					distinct()
+# ring
+symb__ring <- symbols |> filter(class == "bm_iri" | class == "bm_tri") |>
+					select(symbols) |>
+					separate_longer_delim(symbols, delim = ", ") |>
+					distinct()
+data__ring <- pairs |> inner_join(symb__ring, by = c("target" = "symbols")) |>
+					bind_rows(pairs |> inner_join(symb__ring, by = c("found_target" = "symbols"))) |>
 					distinct()
 
 ## Check rules
-# atom_oar
-pct_symb_fail__atom_oar   <- 100 * nrow(data__atom_oar |> filter(result == 0)) / nrow(data__atom_oar)
-class_symb_fail__atom_oar <- data__atom_oar |> filter(result == 0) |> pull(target_class) |> unique()
+# atom_oar / atom_bar
+pct_symb_fail__atom_obar   <- 100 * nrow(data__atom_obar |> filter(result == 0)) / nrow(data__atom_obar)
+class_symb_fail__atom_obar <- data__atom_obar |> filter(result == 0) |> pull(target_class) |> unique()
+# atom_oal / atom_bal
+pct_symb_fail__atom_obal   <- 100 * nrow(data__atom_obal |> filter(result == 0)) / nrow(data__atom_obal)
+class_symb_fail__atom_obal <- data__atom_obal |> filter(result == 0) |> pull(target_class) |> unique()
+# atom_oal_2 / atom_bal_2
+pct_symb_fail__any   <- 100 * nrow(data__any |> filter(result == 0)) / nrow(data__any)
+class_symb_fail__any <- data__any |> filter(result == 0) |> pull(target_class) |> unique()
+# bracket
+pct_symb_fail__br   <- 100 * nrow(data__br |> filter(result == 0)) / nrow(data__br)
+class_symb_fail__br <- data__br |> filter(result == 0) |> pull(target_class) |> unique()
+# single bond
+pct_symb_fail__sb   <- 100 * nrow(data__sb |> filter(result == 0)) / nrow(data__sb)
+class_symb_fail__sb <- data__sb |> filter(result == 0) |> pull(target_class) |> unique()
+# double bond
+pct_symb_fail__db   <- 100 * nrow(data__db |> filter(result == 0)) / nrow(data__db)
+class_symb_fail__db <- data__db |> filter(result == 0) |> pull(target_class) |> unique()
+# triple bond
+pct_symb_fail__tb   <- 100 * nrow(data__tb |> filter(result == 0)) / nrow(data__tb)
+class_symb_fail__tb <- data__tb |> filter(result == 0) |> pull(target_class) |> unique()
+# quadruple bond
+pct_symb_fail__qb   <- 100 * nrow(data__qb |> filter(result == 0)) / nrow(data__qb)
+class_symb_fail__qb <- data__qb |> filter(result == 0) |> pull(target_class) |> unique()
+# aromatic bond
+pct_symb_fail__ab   <- 100 * nrow(data__ab |> filter(result == 0)) / nrow(data__ab)
+class_symb_fail__ab <- data__ab |> filter(result == 0) |> pull(target_class) |> unique()
+# no bond
+pct_symb_fail__nb   <- 100 * nrow(data__nb |> filter(result == 0)) / nrow(data__nb)
+class_symb_fail__nb <- data__nb |> filter(result == 0) |> pull(target_class) |> unique()
+# branch
+pct_symb_fail__branch   <- 100 * nrow(data__branch |> filter(result == 0)) / nrow(data__branch)
+class_symb_fail__branch <- data__branch |> filter(result == 0) |> pull(target_class) |> unique()
+# ring
+pct_symb_fail__ring   <- 100 * nrow(data__ring |> filter(result == 0)) / nrow(data__ring)
+class_symb_fail__ring <- data__ring |> filter(result == 0) |> pull(target_class) |> unique()
